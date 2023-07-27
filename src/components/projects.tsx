cd096ca988408699dc8f2f8719840e346a665c32
Thomas Payne <tepayne97@gmail.com>
2023-07-27 08:31:03 -0700

    correct spelling

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -18,7 +18,7 @@ class Projects extends Component {
                 <h4>AI Application</h4>
                 <p>SpringBoot distributed AI application using the OpenAI API, vector databases and prompt engineering techniques. Repo is currently private.</p>
                 
-                <h4>Decntralized Application Development</h4>
+                <h4>Decentralized Application</h4>
                 <p>Write a variety of small apps to familiarize myself with blockchain related tools and processes.</p>
                 <ul>
                     <li><a href="https://github.com/thpa9933/crowdCoin_dapp">CrowdCoin</a></li>
